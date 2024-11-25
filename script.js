c1e7f70a884b79a7d883ca4f41b3f93d806ee9d5
Alikh-collab <chort445@gmail.com>
2024-11-25 14:59:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// Tapsyrma 1;
+
+// function calc() {
+//     let san1 = +prompt("San engiz:");
+
+//     let tanba = prompt("Tanba engiz:");
+
+//     let san2 = +prompt("San engiz:");
+
+//     let result;
+
+//     if (san1 != 0 && san2 != 0) {
+//         alert("Қате енгізу!");
+//     }
+
+//     switch (tanba) {
+//         case "+":
+//             result = san1 + san2;
+//             break;
+//         case "-":
+//             result = san1 - san2;
+//         case "/":
+//             result = san1 / san2;
+//         case "*":
+//             result = san1 * san2;
+            
+//         default:
+//             break;
+//     }
+//     return result
+
+// }
+// alert(calc())
+
+
+
+
+
+
+// // Tapsyrma 2 Jasalmady
+
+// function Discount() {
+//     let baga = +prompt("Bagasyn jaz:");
+
+//     let qansha = +prompt("Qansha alasyn?:");
+
+//     let qosu = 0;
+
+//     qosu = qosu * qansha / 10;
+
+//     let result;
+
+//     return = (qosu * qansha) / 10;
+
+//     if () {
+        
+//     }
+
+//     return result;
+// }
+// alert(Discount())
+
+
+
+
+// Tapsyrma 3
+
+// function Game() {
+//     let san = +prompt("San jasyr:");
+
+//     let form;
+
+//     while(form !== san){
+//         form = +prompt("Kishi nemese Ulken?");
+
+        
+
+//         if (form  === san) {
+//             alert(san);
+//         }else if ((form - san ) <= 5){
+//         if (form < san) {
+//         alert("Сәл жоғары!");
+
+//         } else {
+//         alert("Сәл төмен!");
+//         }
+         
+//     }else{
+//         if (form < san) {
+//           alert("Жай жоғары!");
+//         } else {
+//           alert("Жай төмен!");
+//         }
+//     }
+    
+// }
+// return result
+// }
+// alert(Game())
